aca029537c4cc78ea428a34caad004895b73072d
mattylenepveu <matthew@lenepveu.com>
2017-05-31 11:53:07 +1000

    All updates for May 24

---
+++ Containers (May 24)/LinkedLists/Entity.cpp
@@ -0,0 +1,21 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+	m_bActive = false;
+}
+
+Entity::~Entity()
+{
+	// Empty
+}
+
+void Entity::SetActive(bool bActive)
+{
+	m_bActive = bActive;
+}
+
+bool Entity::GetActive()
+{
+	return m_bActive;
+}
